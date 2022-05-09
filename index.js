9d0ef46337817e04225be6659a950f751dd87ce7
SomeNiceTables <104398685+SomeNiceTables@users.noreply.github.com>
2022-05-09 01:17:51 -0400

    Added the code and a video

---
+++ index.js
@@ -0,0 +1,33 @@
+const {Client, MessageActionRow, MessageButton, Intents} = require("discord.js")
+const client = new Client({ intents: 32767 })
+
+client.on("ready", () => {
+  console.log("Bot is on")
+})
+
+client.on("messageCreate", (message) => {
+  if (message.content === "hey"){
+    message.reply("Hi")
+  }
+  if (message.content === "!setverify"){
+    const row = new MessageActionRow()
+			.addComponents(
+				new MessageButton()
+					.setCustomId('verify')
+					.setLabel('Verify')
+					.setStyle('SUCCESS'),
+			);
+    message.channel.send({ content: "Click to verify", components: [row]})
+  }
+})
+
+client.on("interactionCreate", async (interaction) => {
+  if (interaction.isButton()){
+    if(interaction.customId === "verify"){
+      interaction.member.roles.add("973004296708841482");
+      interaction.reply({ content: 'You are verified', ephemeral: true });
+    }
+  }
+})
+
+client.login(process.env.TOKEN)
